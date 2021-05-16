dfc1dd80061337f3d8f765a0d6f2c0f4d351c80b
Kavit Shah <kashah@kavits-mbp.lan>
2021-05-15 20:47:01 -0700

    Add an example of STL containers and some reorg of files

---
+++ c++/stl/containers/headers.cpp
@@ -0,0 +1,24 @@
+/**
+ * STL HEADERS
+ */
+
+// Sequence Containers
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <array>
+
+// Associative Containers
+#include <set>
+#include <map>
+
+// Unordered Containers
+#include <unordered_map>
+#include <unordered_set>
+
+// Other Headers
+#include <iterator>
+#include <algorithm>
+#include <numeric>
+#include <functional>
